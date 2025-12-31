80f5304adf913b57ecbaa3a90332c530eacf77e1
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-12-31 17:43:38 +0530

    add developers.google.com

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com", // For Google profile images
       },
+      {
+        protocol: "https",
+        hostname: "developers.google.com", // For Google profile images
+      },
     ],
   },
 };
