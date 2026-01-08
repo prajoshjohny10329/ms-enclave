f90d510897384213891de3e1c049d1507029f5d3
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2026-01-08 18:39:54 +0530

    open check

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,15 @@
-// import { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
-// export default function robots(): MetadataRoute.Robots {
-//   return {
-//     rules: [
-//       {
-//         userAgent: "*",
-//         disallow: "/",
-//       },
-//     ],
-//   };
-// }
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        // disallow: "/",
+
+        // disallow: ["/admin", "/api"],
+      },
+    ],
+  };
+}
